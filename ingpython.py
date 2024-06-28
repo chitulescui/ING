cecf367be92c6c3ba97f08165e315fd15a4ed9d2
Victor Chitulescu <vchitulescu2@gmail.com>
2024-06-28 13:28:24 +0300

    aa

--- ingpython.py
+++ ingpython.py
@@ -21,7 +21,9 @@ try:
         auth_plugin=auth_plugin
     ) as connection:
         create_db_query = "CREATE DATABASE online_movie_rating"
+        create_db_queryy= "DELETE DATABASE online_movie_rating"
         with connection.cursor() as cursor:
+            cursor.execute(create_db_queryy)
             cursor.execute(create_db_query)
             print("a ajuns pana aici")
 except Error as e:
