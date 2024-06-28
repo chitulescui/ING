9b5b9b6f4566afff17cd05e98a2ccdee0c8cfebb
Victor Chitulescu <vchitulescu2@gmail.com>
2024-06-28 13:04:34 +0300

    sasa

--- ingpython.py
+++ ingpython.py
@@ -4,12 +4,13 @@ import os
 
 import mysql.connector
 
-host=os.environ.get("HOST"),
-user=os.environ.get("USER"),
-password=os.environ.get('MYSQL_ROOT_PASSWORD'),
-port=int(os.environ.get("PORT")),
-auth_plugin=os.environ.get("AUTH_PLUGIN")
-database=os.environ.get("DATABASE")
+host = os.environ.get("HOST"),
+user = os.environ.get("USER"),
+password = os.environ.get('MYSQL_ROOT_PASSWORD'),
+port = int(os.environ.get("PORT")),
+auth_plugin = os.environ.get("AUTH_PLUGIN")
+database = os.environ.get("DATABASE")
+
 
 def connect_to_mysql(host, user, password, port, auth_plugin):
     db = mysql.connector.connect(
@@ -21,7 +22,8 @@ def connect_to_mysql(host, user, password, port, auth_plugin):
     )
     return db
 
-def connect_to_database(host, user, password, port, auth_plugin,database):
+
+def connect_to_database(host, user, password, port, auth_plugin, database):
     db = mysql.connector.connect(
         host=host,
         user=user,
@@ -32,14 +34,14 @@ def connect_to_database(host, user, password, port, auth_plugin,database):
     )
     return db
 
+
 def create_a_database():
-    mycursor= db.cursor()
+    mycursor = db.cursor()
     mycursor.execute("CREATE DATABASE testdatabase")
+
 
 connect_to_mysql(host, user, password, port, auth_plugin)
 create_a_database()
-connect_to_database(host, user, password, port, auth_plugin,database)
-
+connect_to_database(host, user, password, port, auth_plugin, database)
 
 print(db)
-
