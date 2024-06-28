5a0b1dd4158ceb6482f3fb3bf53ae41aee4f14bc
Victor Chitulescu <vchitulescu2@gmail.com>
2024-06-28 12:40:52 +0300

    functions

--- ingpython.py
+++ ingpython.py
@@ -4,12 +4,23 @@ import os
 
 import mysql.connector
 
-db = mysql.connector.connect(
-    host=os.environ.get("HOST"),
-    user=os.environ.get("USER"),
-    passwd=os.environ.get('MYSQL_ROOT_PASSWORD'),
-    port=int(os.environ.get("PORT")),
-    auth_plugin=os.environ.get("AUTH_PLUGIN")
-)
+host=os.environ.get("HOST"),
+user=os.environ.get("USER"),
+passwd=os.environ.get('MYSQL_ROOT_PASSWORD'),
+port=int(os.environ.get("PORT")),
+auth_plugin=os.environ.get("AUTH_PLUGIN")
+
+def connect_to_database(host, user, password, port, auth_plugin):
+    db = mysql.connector.connect(
+        host=host,
+        user=user,
+        passwd=password,
+        port=port,
+        auth_plugin=auth_plugin
+    )
+    return db
+connect_to_database(host, user, passwd, port, auth_plugin)
+
 
 print(db)
+
