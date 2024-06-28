709ccbc5af487d44e8b2c95dc0cbe7d2a0f939c0
Victor Chitulescu <vchitulescu2@gmail.com>
2024-06-28 10:59:06 +0300

    dada

--- ingpython.py
+++ ingpython.py
@@ -1,12 +1,12 @@
 print("dog")
 
-import mysql.connector
-
-db = mysql.connector.connect(
-    host="localhost",
-    user="sql1",
-    passwd="Cirica01@@Parolafmm123@@",
-    port="1433"
-)
-
-print(db)
+# import mysql.connector
+# 
+# db = mysql.connector.connect(
+    # host="127.0.0.1",
+    # user="sql1",
+    # passwd="Cirica01@@Parolafmm123@@",
+    # port="1433"
+# )
+# 
+# print(db)
