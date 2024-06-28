386437de406ed6f7ef07e7355329145582ddb33d
Victor Chitulescu <vchitulescu2@gmail.com>
2024-06-28 10:51:40 +0300

    da

--- ingpython.py
+++ ingpython.py
@@ -2,11 +2,11 @@ print("dog")
 
 import mysql.connector
 
-connection = mysql.connector.connect(
-    host="127.0.0.1",
+db = mysql.connector.connect(
+    host="localhost",
     user="sql1",
-    password="Cirica01@@Parolafmm123@@",
+    passwd="Cirica01@@Parolafmm123@@",
     port="1433"
 )
 
-print("dbconnected")
+print(db)
