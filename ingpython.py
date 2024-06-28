fddf9d6cf9c571d4c191d29b8d3b3b14ffa80dcc
Victor Chitulescu <vchitulescu2@gmail.com>
2024-06-28 12:29:43 +0300

    python use envvars

--- ingpython.py
+++ ingpython.py
@@ -5,11 +5,11 @@ import os
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
+    host=os.environ.get("HOST"),
+    user=os.environ.get("USER"),
     passwd=os.environ.get('MYSQL_ROOT_PASSWORD'),
-    port="3306",
-    auth_plugin='mysql_native_password'
+    port=os.environ.get("PORT"),
+    auth_plugin=os.environ.get("AUTH_PLUGIN")
 )
 
 print(db)
