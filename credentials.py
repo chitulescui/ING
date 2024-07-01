0589ab8e637f22b63944fae78ada0245d7fbe1f2
Victor Chitulescu <vchitulescu2@gmail.com>
2024-07-01 04:00:09 +0300

    dasd

--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@ import os
 
 
 SERVER=os.environ.get('HOST')
-PASSWORD=os.environ.get("MSSQL_ROOT_PASSWORD")
+PASSWORD=os.environ.get('MSSQL_ROOT_PASSWORD')
 USERNAME=os.environ.get("USER")
 DATABASE=os.environ.get('DATABASE')
 JSON_NAME=os.environ.get('JSON_NAME')
