8865fcaf39e1a638a285326bfec39850070a4068
Victor Chitulescu <vchitulescu2@gmail.com>
2024-07-01 03:19:02 +0300

    dada

--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@ import os
 
 
 SERVER=os.environ.get('HOST')
-PASSWORD=os.environ.get('$(password)')
+PASSWORD=os.getenv('password')
 USERNAME=os.environ.get("USER")
 DATABASE=os.environ.get('DATABASE')
 TABLE=os.environ.get('TABLE')
