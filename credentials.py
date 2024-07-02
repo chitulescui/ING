32a90c3e0ab196f2012612fdd709efc26c600cf5
Victor Chitulescu <vchitulescu2@gmail.com>
2024-07-02 22:44:26 +0300

    da

--- credentials.py
+++ credentials.py
@@ -1,11 +1,11 @@
 import os
 #Credentials
 
-SERVER=os.environ.get('HOST')
-PASSWORD=os.environ.get('MSSQL_ROOT_PASSWORD')
-USERNAME=os.environ.get("USER")
-DATABASE=os.environ.get('DATABASE')
-JSON_NAME=os.environ.get('JSON_NAME')
+SERVER = os.environ.get('HOST')
+PASSWORD = os.environ.get('MSSQL_ROOT_PASSWORD')
+USERNAME = os.environ.get("USER")
+DATABASE = os.environ.get('DATABASE')
+JSON_NAME = os.environ.get('JSON_NAME')
 
 
 
