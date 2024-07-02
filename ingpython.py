9a150c50999b7117c44b5d9f391249f434cb5d8c
Victor Chitulescu <vchitulescu2@gmail.com>
2024-07-02 23:16:06 +0300

    dasdadsa

--- ingpython.py
+++ ingpython.py
@@ -4,13 +4,13 @@ from os.path import exists
 from credentials import SERVER, PASSWORD, USERNAME, DATABASE, JSON_NAME
 from variables import dict_tables
 dict_tables = {'First':['Alice',30,'New York'],'Second':['Bob', 25,'Los Angeles'], 'Third':['Charlie',22,'Chicago']}
-
+#
 # SERVER=".,1433"
 # PASSWORD="Cirica01@@"
 # USERNAME="sa"
 # DATABASE="trydatabasebun"
 # JSON_NAME="jsontryfilebun.json"
-# #Create the connection to SQL Server
+#Create the connection to SQL Server
 
 def create_connection(server, username, password):
     global connection, cursor                          #Globally declared variables in order to use them in the next functions.
