9401b33434389c9185be138ce85d7d279864855f
Victor Chitulescu <vchitulescu2@gmail.com>
2024-07-03 03:38:27 +0300

    fdsaf

--- ingpython.py
+++ ingpython.py
@@ -1,25 +1,25 @@
 #Import Libraries
 import pyodbc, json, os, random
 from os.path import exists
-from credentials import SERVER, PASSWORD, USERNAME, DATABASE, JSON_NAME, NEW_USERNAME, NEW_PASSWORD, NEW_USER
+# from credentials import SERVER, PASSWORD, USERNAME, DATABASE, JSON_NAME, NEW_USERNAME, NEW_PASSWORD, NEW_USER
 # from variables import dict_tables
 JSON_NAME="ExportJson.json"
 # Additional varibles
 dict_tables = {'First':['Alice',30,'New York'],'Second':['Bob', 25,'Los Angeles'], 'Third':['Charlie',22,'Chicago']}
 
+
+SERVER=".,1433"
+PASSWORD="Cirica01@@"
+USERNAME="sa"
+DATABASE="trydatabasebun"
+JSON_NAME="jdsdsantryfilebun.json"
 #
-# SERVER=".,1433"
-# PASSWORD="Cirica01@@"
-# USERNAME="sa"
-# DATABASE="trydatabasebun"
-# JSON_NAME="jdsdsantryfilebun.json"
-# #
-#
-# SERVER=".,1433"
-# NEW_PASSWORD='logincoco1234@@'
-# NEW_USERNAME='login4'
-# NEW_USER='userlogin4'
-# # # DATABASE="login"
+
+SERVER=".,1433"
+NEW_PASSWORD='logincoco1234@@'
+NEW_USERNAME='login44'
+NEW_USER='userlogin44'
+# # DATABASE="login"
 
 
 #Create the connection to SQL Server
@@ -122,10 +122,10 @@ def export_table():
 
 #Create new login
 def create_login():
-    cursor.execute(f"CREATE LOGIN '{NEW_USERNAME}' WITH PASSWORD = {NEW_PASSWORD};");
-    cursor.execute(f"CREATE USER '{NEW_USER}' FOR LOGIN '{NEW_USERNAME}'")
-    cursor.execute(f"EXEC sp_addsrvrolemember '{NEW_USERNAME}', 'sysadmin';")
-    cursor.execute(f"USE '{DATABASE}'")
+    cursor.execute(f"CREATE LOGIN {NEW_USERNAME} WITH PASSWORD = '{NEW_PASSWORD}';")
+    cursor.execute(f"CREATE USER {NEW_USER} FOR LOGIN {NEW_USERNAME};")
+    cursor.execute(f"EXEC sp_addsrvrolemember {NEW_USERNAME}, 'sysadmin';")
+    cursor.execute(f"USE {DATABASE};")
     print("I`m using the new login")
 
 
